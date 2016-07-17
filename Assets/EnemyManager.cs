342fe3b3ea0dc7a2cec0e01f826616902ee2be86
Matt Messmer <staind2523@gmail.com>
2016-07-16 20:19:51 -0700

    Add basic models and basic collision detection.

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public float Speed = 2;
+
+    [HideInInspector]
+    public Vector3 Direction;
+
+    private void Start()
+    {
+        Direction = Vector3.right;
+
+        GetComponent<Rigidbody2D>().velocity = Direction * Speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+}
---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private void Start()
+    {
+        enemy = (GameObject)Resources.Load("Enemy");
+
+        // Spawn an enemy every 2 seconds
+        InvokeRepeating("SpawnEnemy", 0, 2);
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 startingPosition = new Vector3(-5, 0, 0);
+        GameObject newEnemy = (GameObject)Instantiate(enemy, startingPosition, new Quaternion());
+    }
+
+    private GameObject enemy;
+}
