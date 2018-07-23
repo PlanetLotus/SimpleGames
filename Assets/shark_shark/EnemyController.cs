db0bfe91ff960cf428ef389f0db05a0b2c6dd13a
Matt Messmer <staind2523@gmail.com>
2018-07-22 22:06:15 -0700

    Add basic enemy movement.

---
+++ Assets/shark_shark/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float Speed = 1;
+
+    public Vector3 Direction;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        rigidbody.velocity = -rigidbody.velocity;
+    }
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = Direction * Speed;
+    }
+
+    private Rigidbody2D rigidbody;
+}
